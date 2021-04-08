afc1949efa1dc84e9d1bb61a9712741ede0f44ea
Stu3D <zstruczewski@gmail.com>
2021-04-08 18:20:54 -0400

    initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "../components/header"
+
+export default function About() {
+    return (
+        <div style={{color:'darksalmon'}}>
+        <Header headerText="About Zack (Header)"/>
+        <Header headerText="This is my second Header"/>
+            <p>I am learning to code to better my future</p>
+        </div>
+    )
+}
